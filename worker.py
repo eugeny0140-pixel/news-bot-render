710fd072fca15cc56dd38b431b4f5cf97ae0545b
eugeny0140-pixel <eugeny0140@gmail.com>
2025-11-12 12:14:53 +0300

    Update worker.py

--- worker.py
+++ worker.py
@@ -333,4 +333,4 @@ if __name__ == "__main__":
     while True:
         fetch_and_process()
         logger.info("💤 Sleeping for 10 minutes...")
-        time.sleep(10 * 60)
+        time.sleep(20 * 60)
